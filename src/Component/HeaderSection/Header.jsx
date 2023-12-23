a27c551db3de69c87f935f07306b5b18abc18b1d
swatii7 <swatic946@gmail.com>
2023-12-23 22:02:02 +0530

    work on profile section

--- src/Component/HeaderSection/Header.jsx
+++ src/Component/HeaderSection/Header.jsx
@@ -1,11 +1,68 @@
 import React from 'react'
 import profileImg from '../../assets/Profile/profile-img.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram, faSkype, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-
+import { faFacebookF, faXTwitter, faInstagram, faSkype, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import homeIcon from '../../assets/Profile/home.png'
+import userIcon from '../../assets/Profile/user.png'
+import resumeIcon from '../../assets/Profile/file.png'
+import portfolioIcon from '../../assets/Profile/panel-left.png'
+import serviceIcon from '../../assets/Profile/server.png'
+import contactIcon from '../../assets/Profile/mail.png'
 
 export default function Header() {
+
+  const socialLinkArr= [
+    {
+      href:'#',
+      icon:faXTwitter,
+  },
+  {
+    href:'#',
+    icon:faFacebookF,
+},
+{
+  href:'#',
+  icon:faInstagram,
+},
+{
+  href:'#',
+  icon:faSkype,
+},
+{
+  href:'#',
+  icon:faLinkedinIn,
+},
+
+]
+
+const navLinkArr=[
+  {
+    icon: homeIcon,
+    linkName: 'Home'
+  },
+  {
+    icon: userIcon,
+    linkName: 'About'
+  },
+  {
+    icon: resumeIcon,
+    linkName: 'Resume'
+  },
+  {
+    icon: portfolioIcon,
+    linkName: 'Portfolio'
+  },
+  {
+    icon: serviceIcon,
+    linkName: 'Services'
+  },
+  {
+    icon: contactIcon,
+    linkName: 'Contact'
+  },
+
+]
+
   return (
     
     <header className='headerWrapper'>
@@ -14,23 +71,28 @@ export default function Header() {
                 <img src={profileImg} />
                 <h1 className='text-light fs-4 font-poppins fw-semibold'>Hitesh Chaudhary</h1>
             </div>
-            <div className='text-center mt-3 socialLinks justify-content-between'>
-                <a href='#' className='twitterLink text-light'>
-                <FontAwesomeIcon icon={faTwitter} />
-                </a>
-                <a href='#' className='fbLink text-light'>
-                <FontAwesomeIcon icon={faFacebookF} />
-                </a>
-                <a href='#' className='fbLink text-light'>
-                <FontAwesomeIcon icon={faInstagram} />
-                </a>
-                <a href='#' className='fbLink text-light'>
-                <FontAwesomeIcon icon={faSkype} />
-                </a>
-                <a href='#' className='fbLink text-light'>
-                <FontAwesomeIcon icon={faLinkedin} />
-                </a>
+            <div className='text-center mt-3 socialLinks d-flex justify-content-between'>
+              {socialLinkArr.map((link,index)=>
+              <a key={index} href={link.href} className='twitterLink text-light iconWrapper'>
+              <FontAwesomeIcon icon={link.icon} />
+              </a>
+              )}
+                
             </div>
+            <nav>
+              <ul>
+                {navLinkArr.map((link, index)=>
+                 <li key={index} className='d-flex align-items-center'>
+                 <a className='text-light d-flex align-items-center navActive'>
+                 <img src={link.icon} className='me-2' />
+                   <span>{link.linkName}</span>
+                 </a>
+               </li>
+                )}
+             
+
+              </ul>
+            </nav>
         </div>
     </header>
   )
